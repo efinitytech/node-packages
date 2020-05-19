616e670a8cee6ea9edc02121f1c9c4ee88ecfead
Connor <connorjameslow@gmail.com>
2020-05-19 08:09:36 -0700

    [fix] restore built-out typings

--- qob/src/index.ts
+++ qob/src/index.ts
@@ -1,5 +1,18 @@
 import MutationObserver from 'mutation-observer';
 
+interface ObservedMutationDictionary {
+    childList: MutationRecord[]
+    attributes: MutationRecord[]
+    characterData: MutationRecord[]
+    all(): MutationRecord[]
+    nodes(): Node[]
+}
+
+interface QOb {
+    (qs: string, cb: (rec: ObservedMutationDictionary) => void): MutationObserver
+    for(target: Node): (qs: string, cb: (rec: ObservedMutationDictionary) => void) => MutationObserver
+}
+
 const nodeArray = (list: NodeList) => Array.prototype.slice.call(list);
 
 /**
